e620565cb7756119af7ba89f122ae3d5ae0edd50
Brandon Villa <bornofos@gmail.com>
2020-04-21 20:40:40 -0500

    HTML agregado a App.js

--- src/App.js
+++ src/App.js
@@ -7,18 +7,51 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My First React App</h1>
       </header>
+      <section className="Section">
+        <div className="Personal-info">
+          <div>
+            <span className="Name">Brandon Villa</span>
+            <span className="Work-title">Software Engineer</span>
+          </div>
+          <div>
+            <ul className="Personal-info-list">
+              <li><strong>Telefono:</strong> 3411361316</li>
+              <li><strong>E-mail:</strong> brandon.villa@gmail.com</li>
+              <li><strong>Website:</strong> brandonvilla.dev</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <section className="Section">
+        <div className="About-me">
+          <div>
+            <span className="Subtitle">Acerca de mi</span>
+            <p>
+              Soy una persona apasionada por la tecnología que le gusta aprender
+              nuevas cosas cada día para poder ponerlas en práctica en proyectos
+              personales o dentro del trabajo. bla bla bla...
+          </p>
+          </div>
+          <div className="Skills">
+            <span className="Subtitle">Skills</span>
+            <ul className="Skills-list">
+              <li><strong>HTML</strong></li>
+              <li><strong>CSS</strong></li>
+              <li><strong>JavaScript</strong></li>
+              <li><strong>Node.js</strong></li>
+              <li><strong>TypeScript</strong></li>
+              <li><strong>Python</strong></li>
+              <li><strong>Django</strong></li>
+              <li><strong>React</strong></li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <footer className="Footer">
+        <span>Created by Me</span>
+      </footer>
     </div>
   );
 }
